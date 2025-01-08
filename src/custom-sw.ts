5caeae9a7460b0104de845606ad1b505260488b9
unknown <emanmasoud763@gmail.com>
2025-01-08 16:59:26 +0200

    add caracour

--- src/custom-sw.ts
+++ src/custom-sw.ts
@@ -1,19 +1,19 @@
 import { precacheAndRoute } from "workbox-precaching";
 
-const CACHE_NAME = "workbox-preCache-v2";
+// const CACHE_NAME = "workbox-preCache-v2";
 
-self.addEventListener("install", (event: any) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        "/assets/dd.jpg",
-        "/assets/gg.jpg",
-        "/assets/image.9a65bd94.svg",
-        "/assets/image.b0c2306b.svg",
-        "/assets/images.jpg",
-      ]);
-    })
-  );
-});
+// self.addEventListener("install", (event: any) => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => {
+//       return cache.addAll([
+//         "/assets/dd.jpg",
+//         "/assets/gg.jpg",
+//         "/assets/image.9a65bd94.svg",
+//         "/assets/image.b0c2306b.svg",
+//         "/assets/images.jpg",
+//       ]);
+//     })
+//   );
+// });
 
 precacheAndRoute(self.__WB_MANIFEST);
