beb2ac361e31b9a62c98ff66e929406baee7a195
unknown <emanmasoud763@gmail.com>
2025-01-14 18:07:12 +0200

    save

--- public/custom-sw.js
+++ public/custom-sw.js
@@ -36,15 +36,18 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return (
-        response ||
-        fetch(event.request).then((networkResponse) => {
-          return caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, networkResponse.clone());
-            return networkResponse;
-          });
-        })
-      );
+      // Return cached response if found
+      if (response) {
+        return response;
+      }
+
+      return fetch(event.request).catch(() => {
+        // If the request is a navigation request (like /about, /dashboard),
+        // fallback to the cached index.html
+        if (event.request.mode === "navigate") {
+          return caches.match("/");
+        }
+      });
     })
   );
 });
